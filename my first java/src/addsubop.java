e70ca4554273c0fda9cf91fef8f4eedc42fd46b3
raj47harsha <rajkumarkapadia7@gmail.com>
2023-03-11 07:36:49 +0530

    new files

---
+++ my first java/src/addsubop.java
@@ -0,0 +1,11 @@
+
+public class addsubop {
+  public static void main(String[] args) {
+    System.out.print("Value of addtion is: ");
+    int a = 25;
+    int b = 35;
+    int sum = a + b;
+
+    System.out.println(sum);
+  }
+}
