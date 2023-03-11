e97e4f0016d22f933859acdf1509b82be4b304df
raj47harsha <rajkumarkapadia7@gmail.com>
2023-03-11 22:40:16 +0530

    new files

--- my first java/src/inputdivision2.java
+++ my first java/src/inputdivision2.java
@@ -3,9 +3,21 @@ import java.util.Scanner;
 public class inputdivision2 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    System.out.print("Value of a is: ");
     float   a  = sc.nextFloat();
+    System.out.print("Value of c is: ");
+    float   c  = sc.nextFloat();
+    System.out.print("Value of b is: ");
     float   b  = sc.nextFloat();
-    System.out.println(a/b);
+    
+    // float sum = (a + b)/c;
+    System.out.print("(a+b)/c is: ");
+    System.out.print((a + b)/c);
     sc.close();
+      }
+    
+      
+    
+
   }
-}
+
---
+++ my first java/src/inputname.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class inputname {
+  public static void main(String[] args) {
+    System.out.println("What is your name?");
+    Scanner sc = new Scanner(System.in);
+    String name = sc.next();
+    System.out.println("Hello " + name + " have a good day.");
+    sc.close();
+  }
+}
